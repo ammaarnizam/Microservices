2b23ee71db8a372c1cc78e7139756e55c946497f
Ammaar <ammaarnizam31@gmail.com>
2022-03-20 13:11:52 +0530

    updates to supplier and its controller classes

--- InventoryUI/src/main/java/com/tharishaperera/InventoryUI/Supplier.java
+++ InventoryUI/src/main/java/com/tharishaperera/InventoryUI/Supplier.java
@@ -1,4 +1,86 @@
 package com.tharishaperera.InventoryUI;
 
 public class Supplier {
+
+    private int supplierId;
+    private String supplierName;
+    private String industry;
+    private String supplierEmail;
+    private int supplierTelephone;
+    private String street;
+    private String city;
+    private String country;
+    private String postalCode;
+
+    public int getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(int supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getSupplierEmail() {
+        return supplierEmail;
+    }
+
+    public void setSupplierEmail(String supplierEmail) {
+        this.supplierEmail = supplierEmail;
+    }
+
+    public int getSupplierTelephone() {
+        return supplierTelephone;
+    }
+
+    public void setSupplierTelephone(int supplierTelephone) {
+        this.supplierTelephone = supplierTelephone;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 }
--- InventoryUI/src/main/java/com/tharishaperera/InventoryUI/SupplierController.java
+++ InventoryUI/src/main/java/com/tharishaperera/InventoryUI/SupplierController.java
@@ -1,4 +1,27 @@
 package com.tharishaperera.InventoryUI;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
 public class SupplierController {
+
+    @GetMapping(path = "/search-supplier")
+    public String getAllSearchedUsers(Model model){
+        model.addAttribute("supplier", new Supplier());
+        return "supplier";
+    }
+
+    @PostMapping(path = "/search-supplier")
+    public String searchSupplierById(Model model, @ModelAttribute Supplier supplier){
+        RestTemplate restTemplate = new RestTemplate();
+        Supplier searchedSupplier = restTemplate.getForObject("http://localhost:8080/suppliers/" + supplier.getSupplierId(), Supplier.class);
+        model.addAttribute("searchedSupplier", searchedSupplier);
+        return "supplier";
+    }
+
 }
